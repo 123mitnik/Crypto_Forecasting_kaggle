f1cfc477c024ea65eec794a64eb477f35153b708
Dingxian Cao <caodingxian@gmail.com>
2021-11-29 20:38:44 -0500

    re-organize file

---
+++ script/XGB/xgbmodel.py
@@ -0,0 +1,40 @@
+import xgboost as xgb
+import numpy as np
+# Two new features from the competition tutorial
+def upper_shadow(df):
+    return df['High'] - np.maximum(df['Close'], df['Open'])
+
+def lower_shadow(df):
+    return np.minimum(df['Close'], df['Open']) - df['Low']
+
+# A utility function to build features from the original df
+# It works for rows to, so we can reutilize it.
+def get_features(df):
+    df_feat = df[['Count', 'Open', 'High', 'Low', 'Close', 'Volume', 'VWAP']].copy()
+    df_feat['Upper_Shadow'] = upper_shadow(df_feat)
+    df_feat['Lower_Shadow'] = lower_shadow(df_feat)
+    return df_feat
+def get_Xy_and_model_for_asset(df_train, asset_id):
+    df = df_train[df_train["Asset_ID"] == asset_id]
+    
+    # TODO: Try different features here!
+    df_proc = get_features(df)
+    df_proc['y'] = df['Target']
+    df_proc = df_proc.dropna(how="any")
+    
+    X = df_proc.drop("y", axis=1)
+    y = df_proc["y"]
+    
+    model = xgb.XGBRegressor(
+        n_estimators=500,
+        max_depth=11,
+        learning_rate=0.05,
+        subsample=0.9,
+        colsample_bytree=0.7,
+        missing=-999,
+        random_state=2020,
+        # tree_method='gpu_hist'  # THE MAGICAL PARAMETER
+    )
+    model.fit(X, y)
+
+    return X, y, model
