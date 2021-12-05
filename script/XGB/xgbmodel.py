d3a852c35be4e3723bc1b4e093ce89ef644e44cd
Dingxian Cao <caodingxian@gmail.com>
2021-12-05 04:58:00 -0500

    update xgboost

--- script/XGB/xgbmodel.py
+++ script/XGB/xgbmodel.py
@@ -1,11 +1,19 @@
 import xgboost as xgb
 from lightgbm import LGBMRegressor
 import lightgbm as lgb
+import xgboost as xgb
 import numpy as np
 import gc
+import talib as ta
+import pandas as pd
+import time
+
 
 ######################################################initial model train setting
 # Two new features from the competition tutorial
+def log_return(series, periods=5):
+    return np.log(series).diff(periods=periods)
+
 def upper_shadow(df):
     return df['High'] - np.maximum(df['Close'], df['Open'])
 
@@ -14,100 +22,76 @@ def lower_shadow(df):
 
 # A utility function to build features from the original df
 def get_features(df_feat):
-    ##keep  timestamp
-    #df_feat = df[['Count', 'Open', 'High', 'Low', 'Close', 'Volume', 'VWAP']].copy()
     df_feat['Upper_Shadow'] = upper_shadow(df_feat)
     df_feat['Lower_Shadow'] = lower_shadow(df_feat)
     return df_feat
+def add_features(df, usetalib=True):
+    df['lrtn_1'] = log_return(df['Close'],periods=1)
+    df['lrtn_4'] = log_return(df['Close'],periods=4)
 
-def get_Xy_and_model_for_asset(df_train, asset_id):
-    df = df_train[df_train["Asset_ID"] == asset_id]
-    
-    # TODO: Try different features here!
-    df_proc = get_features(df)
-    df_proc['y'] = df['Target']
-    df_proc = df_proc.dropna(how="any")
-    
-    X = df_proc.drop("y", axis=1)
-    y = df_proc["y"]
-    del df_proc
-    gc.collect()
-    #Xgboost with Scikit-learn API
-    model = xgb.XGBRegressor(
-        n_estimators=500,
-        max_depth=11,
-        learning_rate=0.05,
-        subsample=0.9,
-        colsample_bytree=0.7,
-        missing=-999,
-        random_state=2020,
-        # tree_method='gpu_hist'  # THE MAGICAL PARAMETER
-    )
-    model.fit(X, y)
-
-    return X, y, model
-######################################################new model train setting
-import talib as ta
-import pandas as pd
-
-def log_return(series, periods=5):
-    return np.log(series).diff(periods=periods)
-
-def add_features(df):
-    
-    df["high_div_low"] = df["High"] / df["Low"]
-    df["open_sub_close"] = df["Open"] - df["Close"]
-    
-    df['Open_shift-1'] = df['Open'].shift(-1)
-    df['Open_shift-4'] = df['Open'].shift(-4)
-    df['Open_shift-7'] = df['Open'].shift(-7)
-    df['Open_shift1'] = df['Open'].shift(1)
-    
-    
-    df['close_log1'] = log_return(df['Close'],periods=1)
-    df['close_log5'] = log_return(df['Close'],periods=4)
-    
-    df['ema']   =ta.EMA(df['Open'], timeperiod=20)
-    
-    df['willr'] = ta.WILLR(df['High'], df['Low'],df['Close'], timeperiod=14)
-    ##time info for dependent features
-    times = pd.to_datetime(df["timestamp"],unit="s",infer_datetime_format=True)
-    df["hour"] = times.dt.hour  
-    df["dayofweek"] = times.dt.dayofweek 
-    df["day"] = times.dt.day 
-    
-    df['rsi'] = ta.RSI(df['Close'], timeperiod=14)
-    df['macd'], df['macdsignal'], df['MACD_HIST'] = ta.MACD(df['Close'], fastperiod=12, slowperiod=26, signalperiod=9)
-    
-    df['u_band'], df['m_band'], df['l_band'] = ta.BBANDS(df['Close'], timeperiod=5, nbdevup=2, nbdevdn=2, matype=0)
-    df['adx'] = ta.ADX(df['High'], df['Low'],df['Close'],timeperiod=14)
-    df['DI_plus'] = ta.PLUS_DI(df['High'], df['Low'],df['Close'], timeperiod=14)
-    df['DI_minus'] = ta.MINUS_DI(df['High'], df['Low'],df['Close'], timeperiod=14)
-    #df = df.drop(['timestamp'],axis=1)
-    df['ROCP'] =ta.ROCP(df['Open'])
-    df['momentam'] =ta.MOM(df['Open'])
-    df['APO'] =ta.APO(df['Open'])
-    df['PPO'] =ta.PPO(df['Open'])
-    df['CMO'] =ta.CMO(df['Open'])
-    df['MIDPOINT'] =ta.MIDPOINT(df['Open'])
-    df['TRENDLINE'] =ta.HT_TRENDLINE(df['Open'])
+    if usetalib:
+        df['ema']   =ta.EMA(df['Open'], timeperiod=20)
+        df['willr'] = ta.WILLR(df['High'], df['Low'],df['Close'], timeperiod=14)
+        ##time info for dependent features
+        df['rsi'] = ta.RSI(df['Close'], timeperiod=14)
+        df['macd'], df['macdsignal'], df['MACD_HIST'] = ta.MACD(df['Close'], fastperiod=12, slowperiod=26, signalperiod=9)
+        
+        df['u_band'], df['m_band'], df['l_band'] = ta.BBANDS(df['Close'], timeperiod=5, nbdevup=2, nbdevdn=2, matype=0)
+        df['adx'] = ta.ADX(df['High'], df['Low'],df['Close'],timeperiod=14)
+        df['DI_plus'] = ta.PLUS_DI(df['High'], df['Low'],df['Close'], timeperiod=14)
+        df['DI_minus'] = ta.MINUS_DI(df['High'], df['Low'],df['Close'], timeperiod=14)
+        df['ROCP'] =ta.ROCP(df['Open'])
+        df['momentam'] =ta.MOM(df['Open'])
+        df['APO'] =ta.APO(df['Open'])
+        df['PPO'] =ta.PPO(df['Open'])
+        df['CMO'] =ta.CMO(df['Open'])
+        df['MIDPOINT'] =ta.MIDPOINT(df['Open'])
+        df['TRENDLINE'] =ta.HT_TRENDLINE(df['Open'])
     return df
 
-def get_Xy_and_model_for_asset_1(df_train,asset_id):
+params_xgb = {
+    'learning_rate': 0.05,
+    'max_depth': 11,
+    'n_estimators': 500,
+    'subsample': 0.9,
+    'colsample_bytree':0.7,
+    'missing':-999,
+    'random_state':2020,
+    'objective':  'reg:squarederror'
+}
+def get_Xy_and_model_for_asset(df_train, asset_id):
     df = df_train[df_train["Asset_ID"] == asset_id]
-    df = df.dropna(subset=['Target'])
-    y = df['Target'] 
-    df = df.drop(['Target'],axis=1)
-    df_proc = get_features(df)
-    df_proc = add_features(df_proc)
-    df_proc = df_proc.fillna(-1)
-    df_proc = df_proc.drop(['timestamp', 'Asset_ID','hour','dayofweek', 'day'],axis=1)
-    X= df_proc #.drop("y", axis=1)
-    #print(f'features:{X.columns}')
-    del df_proc
+    df = get_features(df)
+    df = add_features(df)
+    df.dropna(axis = 0, inplace= True)
+
+    y = df.pop('Target') 
+    X = df.drop(['timestamp', 'Asset_ID'],axis=1)
     del df
     gc.collect()
 
+    start_time = time.time()
+    #Xgboost with Scikit-learn API
+    model = xgb.XGBRegressor(**params_xgb)
+    model.fit(X, y)
+    print("--- %s seconds ---" % (time.time() - start_time))
+    return model
+
+######################################################lightgbm_model
+def get_Xy_and_model_for_asset_1(df_train,asset_id):
+    df = df_train[df_train["Asset_ID"] == asset_id]
+    df = df.dropna(subset=['Target'])
+    y = df.pop('Target') 
+    df_proc = get_features(df)
+    del df
+    df_proc = add_features(df_proc)
+    df_proc = df_proc.fillna(-1)
+    X = df_proc.drop(['timestamp', 'Asset_ID'],axis=1)
+    #print(f'features:{X.columns}')
+    del df_proc
+    gc.collect()
+
+    #lightxgboost
     model = LGBMRegressor(n_estimators=1000,num_leaves=500,learning_rate=0.1)
     model.fit(X, y)
 
---
+++ script/memory_usage.py
@@ -0,0 +1,57 @@
+# Memory saving function credit to https://www.kaggle.com/gemartin/load-data-reduce-memory-usage
+
+import numpy as np
+import math
+
+def precision_and_scale(x):
+    max_digits = 14
+    int_part = int(abs(x))
+    magnitude = 1 if int_part == 0 else int(math.log10(int_part)) + 1
+    if magnitude >= max_digits:
+        return (magnitude, 0)
+    frac_part = abs(x) - int_part
+    multiplier = 10 ** (max_digits - magnitude)
+    frac_digits = multiplier + int(multiplier * frac_part + 0.5)
+    while frac_digits % 10 == 0:
+        frac_digits /= 10
+    scale = int(math.log10(frac_digits))
+    return (magnitude + scale, scale)
+
+def reduce_mem_usage(df):
+    """ 
+        iterate through all the columns of a pandas.dataframe and modify the data type
+        to reduce memory usage.
+    """
+    start_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
+
+    for col in df.columns:
+        col_type = df[col].dtype.name
+        if col_type not in ['object', 'category', 'datetime64[ns, UTC]', 'datetime64[ns]']:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                ## control integer
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)  
+            else:
+                ## control float(np.finfo(np.float16).precision)
+                if c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                elif c_min > np.finfo(np.float64).min and c_max < np.finfo(np.float64).max:
+                    df[col] = df[col].astype(np.float64)
+                else:
+                    next
+        print(f'{col_type}->{df[col].dtype.name}')
+
+    end_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
+    print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+
+    return df
